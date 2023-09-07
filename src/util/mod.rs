c47ce4ac5725cf6f8d4613226b1e9a6e492774b7
fwcd <fwcdmail@gmail.com>
2023-09-08 00:08:35 +0200

    Add vec2 and zero trait

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -3,11 +3,15 @@ mod assert;
 mod error;
 mod result;
 mod macros;
+mod vec2;
 mod xml;
+mod zero;
 
 #[cfg(test)]
 pub(crate) use assert::*;
 pub use error::*;
 pub use result::*;
 pub use macros::*;
+pub use vec2::*;
 pub use xml::*;
+pub use zero::*;
