3f55422c47962feb0a0133de0238dcd708ee9c9a
fwcd <fwcdmail@gmail.com>
2023-09-08 13:50:11 +0200

    Use half-open range for bounds
    These are generally more convenient to deal with

--- src/game/board.rs
+++ src/game/board.rs
@@ -1,6 +1,6 @@
 //! Ported from https://github.com/software-challenge/backend/blob/be88340f619892fe70c4cbd45e131d5445e883c7/plugin/src/main/kotlin/sc/plugin2024/Board.kt
 
-use std::ops::RangeInclusive;
+use std::ops::Range;
 
 use crate::util::{Element, Error, Result, Vec2};
 
@@ -13,14 +13,14 @@ pub struct Board {
 }
 
 impl Board {
-    /// The bounding box of the segments as `(min_x..=max_x, min_y..=max_y)`.
-    pub fn bounds(&self) -> (RangeInclusive<i32>, RangeInclusive<i32>) {
-        self.segments.iter().fold(((0..=0), (0..=0)), |(xs, ys), segment| {
+    /// The bounding box of the segments as `(min_x..(max_x + 1), min_y..(max_y + 1))`.
+    pub fn bounds(&self) -> (Range<i32>, Range<i32>) {
+        self.segments.iter().fold(((0..0), (0..0)), |(xs, ys), segment| {
             let center = segment.center;
             let x = center.x() / 2;
             let y = center.y();
-            let xs = (*xs.start()).min(x - 2)..=(*xs.end()).max(x + 2);
-            let ys = (*ys.start()).min(y - 2)..=(*ys.end()).max(y + 2);
+            let xs = xs.start.min(x - 2)..xs.end.max(x + 3);
+            let ys = ys.start.min(y - 2)..ys.end.max(y + 3);
             (xs, ys)
         })
     }
@@ -28,8 +28,8 @@ impl Board {
     /// The size of the bounding box.
     pub fn rectangle_size(&self) -> Vec2<i32> {
         let (xs, ys) = self.bounds();
-        let x = *xs.end() - *xs.start() + 1;
-        let y = *ys.end() - *ys.start() + 1;
+        let x = xs.end - xs.start;
+        let y = ys.end - ys.start;
         Vec2::new(x, y)
     }
 }
