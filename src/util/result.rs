40611de1133dc5c79944d4737d68d146c1569f32
fwcd <fwcdmail@gmail.com>
2023-09-08 16:46:15 +0200

    Only default `Result`

--- src/util/result.rs
+++ src/util/result.rs
@@ -1,4 +1,4 @@
 use super::Error;
 
 /// A shorthand notation for `Result<T, Error>`.
-pub type Result<T> = std::result::Result<T, Error>;
+pub type Result<T, E = Error> = std::result::Result<T, E>;
